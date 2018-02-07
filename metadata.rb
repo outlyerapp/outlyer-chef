d021a1d4d341c92953044caedc66d173ceb3d416
Tom Ashley <tom.ashley@dataloop.io>
2018-02-07 15:08:00 +0000

    new version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@outlyer.com'
 license          'All rights reserved'
 description      'Installs/Configures outlyer agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'apt',      '~> 2.9.2'
 depends 'yum',     '~> 3.8.2'
