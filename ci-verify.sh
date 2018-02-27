a0695923b5c82342f19a1a13b7bb859bb3300261
slve <slve@gmx.com>
2018-02-27 01:18:36 +0000

    catch up folder changes

--- ci-verify.sh
+++ ci-verify.sh
@@ -1,4 +1,4 @@
-cp test/integration/default/integration_test.yml.example test/integration/default/integration_test.yml
+cp data/integration_test.yml.example test/integration/default/integration_test.yml
 sed -i "s/XXX.XXX.XXX-XXX-XXX/$AUTH_TOKEN/" test/integration/default/integration_test.yml
 
 kitchen verify $BOX
