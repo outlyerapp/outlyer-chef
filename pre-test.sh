2a8741e1a13f29e46fdce75a4a4f28fa222084f4
slve <slve@gmx.com>
2018-03-02 15:55:52 +0000

    take version and package_distribution from environment variables

--- pre-test.sh
+++ pre-test.sh
@@ -1,3 +1,3 @@
-sed "s/XXX-XXX-XXX-XXX-XXX/$AGENT_KEY/" .kitchen.yml > .kitchen.local.yml
+sed "s/XXX-XXX-XXX-XXX-XXX/$AGENT_KEY/;s/version: nil/version: $VERSION/;s/package_distribution: stable/package_distribution: $PACKAGE_DISTRIBUTION/" .kitchen.yml > .kitchen.local.yml
 
 sed "s/XXX.XXX.XXX-XXX-XXX/$AUTH_TOKEN/" data/integration_test.yml.template > test/integration_test.yml
