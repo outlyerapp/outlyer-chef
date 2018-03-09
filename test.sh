d1848d3acc0433f28861be01a8e5a6ee848e3493
slve <slve@gmx.com>
2018-03-09 23:12:07 +0000

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-for i in $(kitchen list | cut -d' ' -f1 | sed 1d | grep $BOX); do printf '%-15s\n' "${i/outlyer-agent-/}"; done | parallel --gnu -j3 --plus --tag echo slot:{%} job:{\#}/{= '$_=total_jobs()' =} \&\& kitchen test outlyer-agent-{= s: +:: =}
+kl=$(kitchen list | cut -d' ' -f1 | sed 1d | grep "$BOX" | sed 's/^outlyer-agent-//')
+for i in $kl; do printf '%-15s\n' "$i"; done | parallel --gnu -j3 --plus --tag echo slot:{%} job:{\#}/{= '$_=total_jobs()' =} \&\& kitchen test outlyer-agent-{= s: +:: =}
