519d50d4c5f4f85a1f31f26a634cdf0a611416ec
Tom Ashley <tom.ashley@dataloop.io>
2018-04-20 09:54:15 +0100

    v0.2.3

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures outlyer agent'
 issues_url       'https://github.com/outlyerapp/outlyer-chef/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/outlyerapp/outlyer-chef' if respond_to?(:source_url)
-chef_version     '>= 12.17' if respond_to?(:chef_version)
 supports         'debian'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.3'
 
 depends 'apt',   '~> 2.9.2'
 depends 'yum',   '~> 3.8.2'
