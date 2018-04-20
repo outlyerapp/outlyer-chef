df988ede2eaa18c76b414028c0af70ef9466bb01
Tom Ashley <tom.ashley@dataloop.io>
2018-04-20 09:39:53 +0100

    packages behind SSL url

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,9 +1,13 @@
-default['outlyer']['package_gpg_key'] = 'http://packages.outlyer.com/outlyer-pubkey.gpg'
+default['outlyer']['package_gpg_key'] = 'https://packages.outlyer.com/outlyer-pubkey.gpg'
 default['outlyer']['package_gpg_id'] = 'ACB6D967'
 default['outlyer']['package_distribution'] = 'stable'
 case node['platform_family']
 when 'rhel', 'fedora'
-  default['outlyer']['package_repository'] = "http://packages.outlyer.com/#{node['outlyer']['package_distribution']}/el$releasever/$basearch/"
+  default['outlyer']['package_repository'] = "https://packages.outlyer.com/#{node['outlyer']['package_distribution']}/el$releasever/$basearch/"
+when 'amazon'
+  default['outlyer']['package_repository'] = "https://packages.outlyer.com/#{node['outlyer']['package_distribution']}/el6/$basearch/"
+when 'windows'
+  default['outlyer']['package_repository'] = "https://packages.outlyer.com/#{node['outlyer']['package_distribution']}/windows/"
 when 'debian'
-  default['outlyer']['package_repository'] = "http://packages.outlyer.com/debian"
+  default['outlyer']['package_repository'] = "https://packages.outlyer.com/debian"
 end
