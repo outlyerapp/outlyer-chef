a6479ea6fba6228ff453b8cbe25a8134bb3737d2
slve <slve@gmx.com>
2018-06-28 09:56:03 +0100

    clean up kitchen config + simplify service tests

--- test/agent_test.rb
+++ test/agent_test.rb
@@ -1,5 +1,8 @@
-# might go away when https://github.com/chef/inspec/issues/2483 is implemented
 AMAZONLINUX2 = os[:platform][:name] == 'amazon' && os[:platform][:release] == '2'
+CENTOS7 = os[:platform][:name] == 'centos' && os[:platform][:release].to_f >= 6 && os[:platform][:release].to_f < 7
+WINDOWS = os[:family] == 'windows'
+
+# might go away when https://github.com/chef/inspec/issues/2483 is implemented
 windows_service_ps_cmd = "Get-WmiObject -Class Win32_Service -Filter  \"name = 'outlyer-agent'\" |fl * -Force"
 windows_service_regex = {
   disabled: /StartMode\s*:\sDisabled/,
@@ -30,39 +33,32 @@ control "agent-is-installed" do
 end
 
 control "agent-service-gnu-linux" do
-  only_if { os[:family] != 'windows' and not AMAZONLINUX2 }
   impact 1.0
   title "Agent service is installed and running"
 
-  describe service('outlyer-agent') do
-    it { should be_installed }
-    it { should be_enabled }
-    it { should be_running }
+  if WINDOWS
+    describe powershell(windows_service_ps_cmd) do
+      its('exit_status') { should eq 0 }
+      its('stdout') { should match(windows_service_regex[:auto]) }
+      its('stdout') { should match(windows_service_regex[:running]) }
+    end
+  elsif CENTOS7
+    describe command('status outlyer-agent') do
+      its('exit_status') { should eq 0 }
+      its('stdout') { should match('start/running') }
+    end
+  elsif AMAZONLINUX2
+    describe bash("systemctl status outlyer-agent") do
+      its('exit_status') { should eq 0 }
+    end
+    describe bash("systemctl status outlyer-agent | grep 'Active: active (running)'") do
+      its('exit_status') { should eq 0 }
+    end
+  else
+    describe service('outlyer-agent') do
+      it { should be_installed }
+      it { should be_enabled }
+      it { should be_running }
+    end
   end
 end
-
-control "agent-service-amazonlinux2" do
-  only_if { AMAZONLINUX2 }
-  impact 1.0
-  title "Agent service is installed and running"
-
-  describe bash("systemctl status outlyer-agent") do
-    its('exit_status') { should eq 0 }
-  end
-  describe bash("systemctl status outlyer-agent | grep 'Active: active (running)'") do
-    its('exit_status') { should eq 0 }
-  end
-end
-
-control "agent-service-windows" do
-  only_if { os[:family] == 'windows' }
-  impact 1.0
-  title "Agent service is installed and running"
-
-  describe powershell(windows_service_ps_cmd) do
-    its('exit_status') { should eq 0 }
-    its('stdout') { should match(windows_service_regex[:auto]) }
-    its('stdout') { should match(windows_service_regex[:running]) }
-  end
-end
-
