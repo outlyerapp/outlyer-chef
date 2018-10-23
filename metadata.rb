9a69503422e20f6d4c03ad4c81386d46b88a8c3a
Tom Ashley <tom.ashley@dataloop.io>
2018-10-23 10:15:58 +0100

    v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ issues_url       'https://github.com/outlyerapp/outlyer-chef/issues' if respond_
 source_url       'https://github.com/outlyerapp/outlyer-chef' if respond_to?(:source_url)
 supports         'debian'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 chef_version '>= 12.0'
 
